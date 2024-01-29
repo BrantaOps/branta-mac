5f419de397e8a3dc6d2e7aa1ae40a54334a50e19
kg <keithgardner@icloud.com>
2024-01-29 16:16:33 -0500

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Branta",
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "Branta", dependencies: []),
+        .testTarget(name: "BrantaTests", dependencies: []),
+    ]
+)
