1c96549beb5b9cb321d0ef39b3bb85261f807bcc
kg <keith.gardner@hey.com>
2024-02-03 11:34:27 -0500

    UI

--- branta/ViewControllers/PreferenceViewController.swift
+++ branta/ViewControllers/PreferenceViewController.swift
@@ -11,10 +11,33 @@ import Foundation
 class PreferenceViewController: NSViewController {
 
     @IBOutlet weak var clipboardNotifyBTCAddress: NSSwitch!
+    @IBOutlet weak var runEverySeconds: NSPopUpButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("pref loaded")
+        
+        //TODO - Dict with constants
+        runEverySeconds.addItem(withTitle: "1 Second")
+        runEverySeconds.addItem(withTitle: "5 Seconds")
+        runEverySeconds.addItem(withTitle: "10 Seconds")
+        runEverySeconds.addItem(withTitle: "30 Seconds")
+        runEverySeconds.addItem(withTitle: "60 Seconds")
+        runEverySeconds.addItem(withTitle: "5 Minutes")
+        runEverySeconds.addItem(withTitle: "10 Minutes")
+        runEverySeconds.addItem(withTitle: "30 Minutes")
+        
+        runEverySeconds.selectItem(at: 4)
+        
+        
+        runEverySeconds.target = self
+        runEverySeconds.action = #selector(runEverySelected)
     }
+    
+    @objc func runEverySelected(sender: NSPopUpButton) {
+        if let selectedItem = sender.selectedItem {
+            print("Selected number: \(selectedItem.title)")
+        }
+    }
+    
 }
