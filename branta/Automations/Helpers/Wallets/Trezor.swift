6c5d4f3f7fc11e6e84ce2dccc26b4d38a1d98b1e
kg <keithgardner@icloud.com>
2024-02-12 14:01:08 -0500

    trezor installers for 23.12.3

--- branta/Automations/Helpers/Wallets/Trezor.swift
+++ branta/Automations/Helpers/Wallets/Trezor.swift
@@ -65,7 +65,16 @@ class Trezor: Wallet {
             "R188nhklYdnU498O6wKkQjwtDG/BAzfYQqdWRcD0i+MfMTb0o4Ba1haopucqqTayQlCmPqmSqbVA8io07WCRgA==": "Trezor-Suite-24.1.2-mac-x64.dmg",
             "WsVY05OoY0hshOV2Xsar3ooBAZoVDEKXy2YTWyUDS+DnbZsu2zy2szNEqESrVPPRbdRH1T0OF3A1ypTuOJsbtQ==": "Trezor-Suite-24.1.2-mac-arm64.dmg",
             "/D3mwyF00YWgDVq0GNDyegc8mLF63cxCOe/vnpGyLz9/Oj5aBl3oG32cj+c8e11+eHYigkKb72nFz5zBoPx8Bw==": "Trezor-Suite-24.1.2-linux-arm64.AppImage",
-            "ehIIOksVzKLGYs6GNZ8w5XvellFRb9sHVORS7MOXmwbbikjgkNX/nlfjwmUKOysxI4PwPzIbqtuX2GYyC9lXHw==": "Trezor-Suite-24.1.2-linux-x86_64.AppImage"
+            "ehIIOksVzKLGYs6GNZ8w5XvellFRb9sHVORS7MOXmwbbikjgkNX/nlfjwmUKOysxI4PwPzIbqtuX2GYyC9lXHw==": "Trezor-Suite-24.1.2-linux-x86_64.AppImage",
+            
+            "miD4SzLzETW+2cLj2VwRy9ZuL8nTw8kKG1uU9EmLRJPukyhY9Od3yeMmxztEafodqE7wv6TxEx4Fi/XIbyC2lQ==": "Trezor-Suite-23.12.3-linux-arm64.AppImage",
+            "IZZmRaWU0POy+Ufx6Ct4/fLzRy+NbSmI+YqdMZd9uTUh0jhPf3BQ2JLwANlUUFZzM+USSTUCjFl0PQ4QQpjI6Q==": "Trezor-Suite-23.12.3-linux-x86_64.AppImage",
+            "SAMaH+VK/I0puVT3vKVUd/1oYPiShglHiVFE2C4zrBSEvVS9UJsz24nkf5LvnCcFXq/K9Qh7evdCKphFE+iW4Q==": "Trezor-Suite-23.12.3-mac-x64.zip",
+            "+bJAWY9EmC7GLMKWYWRvbgm6oB96/DDgs5MGYqzjm4tp0g/kdm4dwRSikZzNMAHHvKTupFEa16/QVFEiXOgTCA==": "Trezor-Suite-23.12.3-mac-arm64.zip",
+            "Po2aNoZf6Z5bIq7uQRKjcWxhRL3qG8HtSquR4nXIPYeJdk6eahMy/V83T+SQ0kaSqLtdhAPYxDxYvsFSxbhHTQ==": "Trezor-Suite-23.12.3-mac-x64.dmg",
+            "7HQX/iMOS65Y8adkQSoHb5tQxYQqbTGiFfIg0iXsLwi9jD1HiPzFXthtqaSrgd7CCf6nXS9RXmmCtiLJhAiR2A==": "Trezor-Suite-23.12.3-mac-arm64.dmg",
+            "t34O8+KoBdEFyE3gUqOjAmv2D6EugAAO4tDWQQMIc467VYw+/bmp1wJg3Pf+EZZBS7FgIO44B5/KgO47HsWMog==": "Trezor-Suite-23.12.3-win-x64.exe"
+            
             
         ]
     }
