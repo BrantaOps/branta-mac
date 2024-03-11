e219a4261a3d73670272edafde84a435f6e70589
kg <keithgardner@icloud.com>
2024-03-11 10:01:46 -0400

    rip out sparkle, sentry

--- branta/AppDelegate.swift
+++ branta/AppDelegate.swift
@@ -7,7 +7,6 @@
 
 import Cocoa
 import Countly
-import Sparkle
 
 let FONT                    = "Avenir"
 let GOLD                    = "#B1914A"
@@ -103,7 +102,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func start() {
         startCountly()
-        startSparkle()
                 
         setupMenu(status: ACTIVE)
         if notificationManager == nil {
@@ -137,10 +135,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         config.appKey = "ccc4eb59a850e5f3bdf640b8d36284c3bce03f12"
         config.host = "https://branta-0dc12e4ffb389.flex.countly.com"
         Countly.sharedInstance().start(with: config)
-    }
-    
-    func startSparkle() {
-        SUUpdater.shared().automaticallyChecksForUpdates = true
     }
     
     func wipeDefaults() {
