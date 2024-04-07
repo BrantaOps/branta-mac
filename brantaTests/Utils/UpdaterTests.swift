d3129af503715802e2a6cb754f0fc3c0ca7d4dfe
kg <keith.gardner@hey.com>
2024-04-07 11:26:28 -0400

    UpdaterTests.

---
+++ brantaTests/Utils/UpdaterTests.swift
@@ -0,0 +1,25 @@
+//
+//  UpdaterTests.swift
+//  BrantaTests
+//
+//  Created by Keith Gardner on 4/7/24.
+//
+
+
+import XCTest
+@testable import Branta
+
+class UpdaterTests: XCTestCase {
+    
+    func testCheckForUpdatesWithLatestVersionAvailable() {
+        let expectation = self.expectation(description: "Check for updates")
+        
+        Updater.checkForUpdates { (needsUpdate, latestVersion) in
+            XCTAssertTrue(needsUpdate)
+            XCTAssertEqual(latestVersion, "1.11.0")
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+}
