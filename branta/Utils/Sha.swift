54baeabbddd58627afb01c4e4c6d3543d8460141
kg <keith.gardner@hey.com>
2024-04-23 09:53:21 -0400

    Sha dir.

--- branta/Utils/Sha.swift
+++ branta/Utils/Sha.swift
@@ -8,6 +8,7 @@
 import CryptoKit
 import Foundation
 
+// Single File API
 class Sha {
     class func sha256(at filePath: String) -> String {
         do {
@@ -39,6 +40,47 @@ class Sha {
         } catch {
             BrantaLogger.log(s: "sha512() Error reading file: \(error)")
             return Data()
+        }
+    }
+}
+
+// Directory API
+extension Sha {
+    
+    
+    // TODO - does this exactly match command line?
+    // Or does this skip symlinks whereas command line does not?
+    
+    // 1. Traverse directory, skip symlinks, hash files
+    // 2. Combine hashes
+    // 3. SHA256 the combined hashes
+    class func sha256ForDirectory(atPath path: String) -> String? {
+        var concatenatedHashes = ""
+
+        guard let enumerator = FileManager.default.enumerator(atPath: path) else {
+            return nil
+        }
+
+        for e in enumerator {
+            if let node = e as? String {
+                let fullPath = path + "/" + node
+
+                let isDirectory = (enumerator.fileAttributes?[.type] as? FileAttributeType) == .typeDirectory
+                let isSymLink = (enumerator.fileAttributes?[.type] as? FileAttributeType) == .typeSymbolicLink
+
+                if !isDirectory && !isSymLink {
+                    print("Hashing: \(fullPath)")
+                    let hash = sha256(at: fullPath)
+                    concatenatedHashes += hash
+                }
+            }
+        }
+
+        if let data = concatenatedHashes.data(using: .utf8) {
+            let hashed = SHA256.hash(data: data)
+            return hashed.compactMap { String(format: "%02x", $0) }.joined()
+        } else {
+            return nil
         }
     }
 }
