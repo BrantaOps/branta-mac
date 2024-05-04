619ac6d2a029e600f1bca1072350e23b8a219091
kg <keith.gardner@hey.com>
2024-05-04 09:19:09 -0500

    Drafting Clipboard Guardian View.

---
+++ branta/Views/ClipboardGuardianView.swift
@@ -0,0 +1,38 @@
+//
+//  ClipboardGuardianView.swift
+//  Branta
+//
+//  Created by Keith Gardner on 5/4/24.
+//
+
+import Cocoa
+import Foundation
+
+class ClipboardGuardianView: NSView {
+
+
+    private let LIVE_COLOR = NSColor.darkGray.cgColor
+    private let IDLE_COLOR = NSColor(hex: GRAY)?.cgColor
+    
+    private let MIN_HEIGHT: CGFloat = 100
+    private let MIN_WIDTH: CGFloat = 200
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        self.wantsLayer = true
+        self.layer?.backgroundColor = IDLE_COLOR
+        self.layer?.cornerRadius = 8
+
+        registerForDraggedTypes([NSPasteboard.PasteboardType.URL, NSPasteboard.PasteboardType.fileURL])
+    }
+    
+    
+    override var intrinsicContentSize: NSSize {
+        var size = super.intrinsicContentSize
+        size.width = max(size.width, MIN_WIDTH)
+        size.height = max(size.height, MIN_HEIGHT)
+        
+        return size
+    }
+}
